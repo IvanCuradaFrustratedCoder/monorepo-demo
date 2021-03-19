8c237434f63d138831abc044ce9bb9a86892c2bf
Ivan <zvpcurada@globe.com.ph>
2021-03-19 08:54:57 +0800

    removed unused imports

--- apps/memo-app/src/app/app.tsx
+++ apps/memo-app/src/app/app.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styles from './app.module.css';
-import logo_memo from '../assets/img/logo_memo.png';
 
-import { AppHeader, Navigation } from '@monorepo-demo/memo-app/layouts';
 
 export function App() {
     return (
